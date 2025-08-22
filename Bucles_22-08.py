db2d935de32b25a756b8f582a189423eb6f54492
Sebastian <jsebastianjm2010@gmail.com>
2025-08-22 17:29:29 -0500

    ESTE ES EL TALLER DE BUCLES

---
+++ Bucles_22-08.py
@@ -0,0 +1,67 @@
+
+#  EJEMPLOS
+
+# num=int(input("INGRESE ALGUN NUMERO: "))
+# while num>0:
+#     print(f"{num}")
+#     num-=1
+# print("TERMINO EL CONTEO")   
+
+
+# num=int(input("INGRESE ALGUN NUMERO DE LA TABLA DE MULTIPLICAR: "))
+# inicio=1
+
+# print(f"\INICIA EL CONTADOR EN 1 {num}: ")
+
+# while inicio <=10:
+#     print(f"{num} * {inicio} = {num * inicio}")
+#     inicio +=1
+
+
+# x = 5
+# while True:
+#     x -= 1
+#     print(x)
+#     if x == 0:
+#         break
+#     print("FIN DEL BUCLE")
+
+#  EJERCICIOS CON WHILE, CONDICIONALES Y ESTRUCTURAS
+
+# num=int(input("INGRESE ALGUN NUMERO ENTERO: "))
+# while num == 0:
+
+
+
+
+
+#    2
+
+# password = ""
+# while password != "python123":
+#     password=input("INGRESE LA CONTRASEÑA: ")
+# print("CONTRASEÑA CORRECTA   ACCESO CONCEDIDO")
+
+#    3
+
+# lis=[]
+# pro=int("INGRESA LOS PRODUCTOS DESEADOS: ")
+# while True:
+#     lis.append(pro)
+#     if pro == "FIN":
+#         break
+#     print(f"ESTA ES LA LISTA DE TUS COSAS: {lis}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
